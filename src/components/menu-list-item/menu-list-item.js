a3bd31f340b4508c4b67b4b4b43fb30bbcb1b727
Azat <aza.er.01@mail.ru>
2019-07-02 00:26:33 +0600

    15

--- src/components/menu-list-item/menu-list-item.js
+++ src/components/menu-list-item/menu-list-item.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './menu-list-item.scss';
+import imgSalad from './img/salad.png';
+import imgPizza from './img/pizza.png';
+import imgMeat from './img/meat.png';
 
 const MenuListItem = ({menuItem}) => {
-    const {title, price, url, category, icon} = menuItem;
+    const {title, price, url, category} = menuItem;
 
+    let img;
+
+    if (category === 'salads') {
+        img = imgSalad;
+    } else if (category === 'pizza') {
+        img = imgPizza;
+    } else if (category === 'meat') {
+        img =imgMeat;
+    }
 
     return (
         <li className="menu__item">
             <div className="menu__title">{title}</div>
             <img className="menu__img" src={url} alt={title}></img>
             <div className="menu__category">Category: <span>{category}</span></div>
-            <div className="menu__price">Price: <span>{price}$</span><img src={icon} alt={title}></img></div>
+            <div className="menu__price">Price: <span>{price}$<img src={img} alt={title}></img></span></div>
             <button className="menu__btn">Add to cart</button>
         </li>
     )
